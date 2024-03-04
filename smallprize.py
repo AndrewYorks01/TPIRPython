a49201434b69faaab331edc6ae24dfeeadb02900
Andrew Yorks <95833575+AndrewYorks01@users.noreply.github.com>
2024-03-04 16:01:06 -0500

    Improved higher/lower generating function

--- smallprize.py
+++ smallprize.py
@@ -8,8 +8,12 @@ def generate_hl_price(price, higher_or_lower):
     val = int(higher_or_lower)
     result = p
     if (val == 0): # generate a fake price that's too low (HIGHER)
-        while (abs(p-result) < 5):
-            result = random.randint(8, p)
+        if (p < 50):
+            while (abs(p-result) < 5):
+                result = random.randint(8, p)
+        else:
+            while (abs(p-result) < 5):
+                result = random.randint(50, p)           
     else:
         while (abs(p-result) < 5):
             result = random.randint(p, p+20)
